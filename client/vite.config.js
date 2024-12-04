d1ab85902916898bec43d5e68787b11c37ba5c27
Alan <alan.williams.brito@gmail.com>
2024-12-04 14:05:59 -0300

    Modified files for deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,23 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
+
+
 export default defineConfig({
+  base: "",
+  root,
   plugins: [react()],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        rootpage: resolve(root, 'rootpage', 'index.html')
+      }
+    }
+  }
 })
